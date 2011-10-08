5e18a4355fe3b66d3e44c4f7ed09ee75909a90c6
Argel Arias <levhita@gmail.com>
2011-10-08 13:33:10 -0500

    details

---
+++ details/circulacion.php
@@ -0,0 +1,3 @@
+<p>Todo ciclista deberá, por su seguridad, circular en cualquier momento por el extremo
+derecho de la calle así como señalar con el brazo correspondiente hacia donde girará en
+caso de necesitar cambiar de calle.</p>
