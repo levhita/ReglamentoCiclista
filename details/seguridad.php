b6aea53702054bc7ad9322870430e453c65ecbcd
Argel Arias <levhita@gmail.com>
2011-10-08 11:56:59 -0500

    seguridad

---
+++ details/seguridad.php
@@ -0,0 +1,6 @@
+<p>Usar casco puede ser molesto, pero te protege de accidentes,
+anteriormente se realizaron fuertes campañas para que automovilistas usaran el cinturon de seguridad
+ahora es tu turno de tomar conciencia y no salir a la calle sin casco.</p>
+
+<p>Reflejantes, lamparas chalecos y cualquier otro aparatejo que te haga más visible de noche es siempre
+necesario, los conductores no están acostumbrados a ver en la obscuridad.</p>
--- rule.php
+++ rule.php
@@ -1,4 +1,4 @@
 <div class="rule">
-  <h2><a href="/regla/<?=$rule['id']?>/" title="Detalles"><?=$rule['name']?></a></h2>
+  <h2><a href="/regla/<?=$rule['id']?>/" title="Detalles"><?=$rule['name']?>:</a></h2>
   <p><?=$rule['description']?>
 </div>
